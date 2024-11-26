2db105b80cd7a3a436939cd5c4928af4fbf7e13f
fermeridamagni <fermeridamagni@outlook.com>
2024-11-25 23:37:19 -0600

    feat(Card Component): Added new Component

---
+++ src/components/Card.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+import { cn } from "../utils/cn";
+
+export interface CardProps extends React.HTMLAttributes<HTMLDivElement> {
+  children: React.ReactNode;
+}
+
+export const Card = React.forwardRef<HTMLDivElement, CardProps>(
+  ({ ...props }, ref) => {
+    return (
+      <div
+        ref={ref}
+        className={cn(
+          "w-full overflow-hidden border bg-gradient-to-bl from-light to-white dark:from-dark dark:to-neutral-900 border-light dark:border-neutral-800 backdrop-blur-sm shadow-md rounded-2xl p-6",
+          props.className
+        )}
+        {...props}
+      >
+        {props.children}
+      </div>
+    );
+  }
+);
+
+Card.displayName = "Card";
+
+export default Card;
--- src/main.ts
+++ src/main.ts
@@ -5,3 +5,6 @@ export type { ButtonProps } from "./components/Button";
 
 export { Input } from "./components/Input";
 export type { InputProps } from "./components/Input";
+
+export { Card } from "./components/Card";
+export type { CardProps } from "./components/Card";
